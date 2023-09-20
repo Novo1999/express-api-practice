e4bc0752dc0a524f4dbdf62d846006dab2a1b00e
Novodip Mondal <neptune.navo@gmail.com>
2023-09-20 18:18:39 +0600

    backend

--- controllers/music.js
+++ controllers/music.js
@@ -1,30 +1,50 @@
 const Music = require('../models/Music')
 const { StatusCodes } = require('http-status-codes')
+const NotFoundError = require('../errors/not-found')
 
 const getAllMusic = async (req, res) => {
-  const musics = await Music.findOne({ createdBy: req.user.userId }).sort(
+  const musics = await Music.find({ createdBy: req.user.userId }).sort(
     'createdAt'
   )
-
+  if (musics.length < 1) {
+    return res.json({ msg: 'No Music found' })
+  }
   return res.status(StatusCodes.OK).json({ musics })
 }
 
-const getSingleMusic = (req, res) => {
-  res.send('Single musics')
+const getSingleMusic = async (req, res) => {
+  const { id } = req.params
+  const music = await Music.findById(id)
+  if (!music) {
+    throw new NotFoundError('Music not found')
+  }
+  return res.status(StatusCodes.OK).json({ music })
 }
 
 const postMusic = async (req, res) => {
   req.body.createdBy = req.user.userId
   const music = await Music.create(req.body)
-  res.status(StatusCodes.CREATED).json({ music })
+  return res.status(StatusCodes.CREATED).json({ music })
 }
 
-const deleteMusic = (req, res) => {
-  res.send('delete music')
+const deleteMusic = async (req, res) => {
+  const { id } = req.params
+  const music = await Music.findOneAndRemove({ _id: id })
+  if (!music) {
+    throw new NotFoundError('Music not found')
+  }
+  return res
+    .status(StatusCodes.OK)
+    .json({ msg: `Successfully deleted ${music.track} by ${music.artist}` })
 }
 
-const updateMusic = (req, res) => {
-  res.send('update musics')
+const updateMusic = async (req, res) => {
+  const { id } = req.params
+  const music = await Music.findOneAndUpdate({ _id: id })
+  if (!music) {
+    throw new NotFoundError('Music not found')
+  }
+  return res.status(StatusCodes.OK).json({ msg: 'Music successfully updated' })
 }
 
 module.exports = {
